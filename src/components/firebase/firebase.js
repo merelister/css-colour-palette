47fd82c3c1aab19e1ec46124eff97e75998e7b2c
meredith <listermeredith@gmail.com>
2020-10-31 16:48:42 -0700

    node modules for colour picker

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -1,8 +1,15 @@
 import app from 'firebase/app';
 
-const fireBaseConfig = {
-    // Firebase config goes here
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyCULwlLw0lSmvj8M4EOncA7CAzIjeN_YKg",
+    authDomain: "css-colour-palette.firebaseapp.com",
+    databaseURL: "https://css-colour-palette.firebaseio.com/",
+    projectId: "css-colour-palette",
+    storageBucket: "css-colour-palette.appspot.com",
+    messagingSenderId: "476789974619",
+    appId: "1:476789974619:web:1d32a30ecb6b226895417e",
+    measurementId: "G-TZ2T057476"
+  };
 
 class Firebase {
     constructor() {
